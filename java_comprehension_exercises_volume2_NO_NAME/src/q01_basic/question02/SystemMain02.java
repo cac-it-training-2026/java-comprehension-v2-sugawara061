f4ce86b96af8eb67da282b7e09f2007bb1e4e1fb
riosu <riosu@C010423324.zipangu.cac.co.jp>
2026-04-28 16:07:41 +0900

    0428 q01_basic.question04まで完了
    理解できた点
    ・オブジェクト作成
    ・フィールド代入
    不明な点、調査中
    
    ・継承
    ・オーバーライド

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question02/SystemMain02.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question02/SystemMain02.java
@@ -3,7 +3,24 @@ package q01_basic.question02;
 public class SystemMain02 {
 
 	public static void main(String[] args) {
-		//TODO ここから実装する
+
+		Member member = new Member();
+
+		System.out.println("---SHOW DATA---");
+		System.out.println("***MENBER DATA***");
+		member.setName("Miura Manabu");
+		member.setAge(30);
+		member.setRank(1);
+
+		member.showMember();
+
+		System.out.println("*****************");
+		System.out.println(">>>RANK UP>>>");
+		member.rankUp();
+
+		System.out.println("---GET RANK---");
+		System.out.println("newRank:" + member.getRank());
 
 	}
+
 }
